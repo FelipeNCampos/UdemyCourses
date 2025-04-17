da56c5a0e0549562e0dbca32945f3863c9bcee43
Felipe Nunes <140024606+FelipeNCampos@users.noreply.github.com>
2025-04-16 22:50:26 -0300

    20

--- day 20/SnakeGame.py
+++ day 20/SnakeGame.py
@@ -6,7 +6,6 @@ import time
 class SnakeGame:
     screen = Screen()
     screen.tracer(0)
-    screen.tracer(0)
 
 
     screen.title("Snake Game") 
@@ -15,28 +14,44 @@ class SnakeGame:
 
     pos = [(0,0), (-20,0), (-40,0)]
     corpo = []
+    def __init__(self):
+            
+        for c in self.pos:
+            temp = Turtle()
+            temp.color("green")
+            
+            temp.shape("square")
+            temp.penup()
+            temp.goto(c)
+            self.corpo.append(temp)
 
-    for c in pos:
-        temp = Turtle()
-        temp.color("green")
-        
-        temp.shape("square")
-        temp.penup()
-        temp.goto(c)
-        corpo.append(temp)
+            
+        self.screen.update()
 
-        
-    screen.update()
+        flag = 1
 
-    while True:
-        for c in corpo:
-            c.forward(20)
+        self.screen.listen()
+        while flag:
+            for c in range(len(self.corpo)-1, 0, -1):
+                x = self.corpo[c-1].xcor()
+                y = self.corpo[c-1].ycor()
+                self.corpo[c].goto(x,y)
+            
+
+            self.corpo[0].forward(20)
+            
+            self.screen.update()
+            
+            if self.corpo[0].xcor() > 500 or self.corpo[0].xcor() < -500 or self.corpo[0].ycor() > 300 or self.corpo[0].ycor() < -300:
+                flag = 0
+                print("Game Over")
+
             time.sleep(0.1)
-        screen.update()
+
+        self.screen.exitonclick()
 
 
-
-    screen.exitonclick()
+game = SnakeGame()
         
     
         
