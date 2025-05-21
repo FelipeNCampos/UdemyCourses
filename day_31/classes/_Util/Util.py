9414bbeb54c6913997d0bf759505d4b82bd16b25
Felipe Nunes <140024606+FelipeNCampos@users.noreply.github.com>
2025-05-21 18:41:16 -0300

    ..

--- day_31/classes/_Util/Util.py
+++ day_31/classes/_Util/Util.py
@@ -14,11 +14,11 @@ class Util:
     @staticmethod
     def addPalavraConhecida():
         try:
-            with open("../../data/know.json","r") as file:
+            with open("../../data/teste.json","r") as file:
                 print("Arquivo de palavras conhecidas ja existe")
 
         except FileNotFoundError:    
             print("Arquivo de palavras conhecidas não encontrado, prosseguindo para criação:")
-            os.makedirs(os.path.dirname("../../data/know.json"), exist_ok=True)
-            with open("../../data/know.json", "w") as file:
+            os.makedirs(os.path.dirname("../../data/teste.json"), exist_ok=True)
+            with open("../../data/teste.json", "w") as file:
                 json.dump([], file, indent=4)
