dba677b2a34bfa518ffd26b78685d2d65b862f63
Felipe Nunes <140024606+FelipeNCampos@users.noreply.github.com>
2025-06-02 14:27:40 -0300

    day 36 done

--- day_36/main.py
+++ day_36/main.py
@@ -22,22 +22,60 @@ def readJsonasDict(caminho):
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
 #HINT 2: Work out the value of 5% of yerstday's closing stock price. 
-
-params = {
-    "apikey":readJsonasDict("./.config")["stockkey"],
-    "function":"TIME_SERIES_DAILY",
-    "symbol":STOCK,
-}
-
-
+def getPorcent():
+    params = {
+        "apikey":readJsonasDict("./.config")["stockkey"],
+        "function":"TIME_SERIES_DAILY",
+        "symbol":STOCK,
+    }
 
 
+    day = datetime.datetime(2025,5,30)
+    previus = datetime.datetime(day.year,day.month,day.day-1)
 
-response = requests.get(STOCK_ENDPOINT,params=params).json()
+
+    day = str(day)[:10]
+    previus = str(previus)[:10]
+
+
+    response = requests.get(STOCK_ENDPOINT,params=params).json()
+
+
+    now = float(response["Time Series (Daily)"][day]["1. open"])
+    antes = float(response["Time Series (Daily)"][previus]["4. close"])
+
+
+    return round(100*antes/now,4)
 
 
 
-print(response)
+def getNews():
+    params2 = {
+        "apiKey":"696b27f058ca4f0fbd4b439c60bc3729",
+        "q":COMPANY_NAME
+    }
+
+    response2 = requests.get(NEWS_ENDPOINT,params=params2).json()
+
+    data = response2.get("articles")[:3]
+
+    print(data[0]["title"])
+    print(data[1]["title"])
+    print(data[2]["title"])
+
+
+    
+
+
+
+
+
+getNews()
+
+if (getPorcent() >= 105):
+    getNews()
+
+
 
 ## STEP 2: Use https://newsapi.org/docs/endpoints/everything
 # Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
