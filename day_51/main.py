d7cf02e31bfe8aafcde3a9d679572584a2bb001c
Felipe Nunes <felipe.n.cmp@gmail.com>
2026-04-01 02:05:44 -0300

    day _51

--- day_51/main.py
+++ day_51/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
-
+import time 
 twiter_url="https://x.com/home"
 speedTeste_url="https://www.speedtest.net/pt"
 
@@ -44,16 +44,22 @@ config.add_argument(f"--user-data-dir={user_data_dir}")
 config.add_experimental_option("detach", True)
 
 drive = webdriver.Chrome(options=config)
-drive.implicitly_wait(10)
+drive.implicitly_wait(3)
 
 
 drive.get(speedTeste_url)
 
 drive.find_element(By.CLASS_NAME, "start-text").click()
 
-download = drive.find_element(By.CLASS_NAME, "result-data u-align-left").text
-upload = drive.find_element(By.CLASS_NAME, "result-item result-item-first-class result-item-upload updated test-mode-multi").find_element("result-data u-align-left").text
+time.sleep(40)
+download = float(drive.find_element(By.CLASS_NAME, "result-data-large").text)
+upload = float(drive.find_element(By.CLASS_NAME, "upload-speed").text)
+
 
 print(f"download = {download}, upload = {upload}")
+
+if download < 600 or upload < 20:
+     drive.get(twiter_url)
+     drive.find_element(By.CLASS_NAME, "public-DraftStyleDefault-block").send_keys(f"I pay for 600/20 and only receive {download}/{upload}!")
 
 
